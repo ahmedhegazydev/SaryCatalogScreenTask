915cf51b2b3a3dded63282381d483c1beccd9dee
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-08-07 17:08:59 +0200

    solid some methods

--- app/src/main/java/com/example/sarycatalogtask/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/sarycatalogtask/ui/fragments/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.viewModels
 import com.example.sarycatalogtask.R
 import com.example.sarycatalogtask.data.banners.BannerResult
 import com.example.sarycatalogtask.data.banners.BannersData
+import com.example.sarycatalogtask.data.catalog.CatalogsData
 import com.example.sarycatalogtask.data.catalog.Data
 import com.example.sarycatalogtask.databinding.FragmentHomeBinding
 import com.example.sarycatalogtask.domain.states.State
@@ -52,20 +53,9 @@ class HomeFragment(override val layoutRes: Int = R.layout.fragment_home) :
             when (state) {
                 is State.Success -> {
                     state.data.let {
-//                        it.result.map {
-//                            Log.e(TAG, "setupBannersObserver: ${it.id}")
-//                        }
-//                        it.result.toMutableList().apply {
-//                            repeat(5) { pos ->
-//                                add(this[pos].copy())
-//                            }
-//                            adapterBanners.setItemAndNotify(this)
-//                        }
 
                         adapterTopTopBanners.setShimmerEnabled(false)
                         adapterTopTopBanners.setItemAndNotify(it.result.toMutableList())
-//                        shimmerTopBanners.hide()
-//                        rvBanners.show()
 
                     }
                 }
@@ -92,41 +82,20 @@ class HomeFragment(override val layoutRes: Int = R.layout.fragment_home) :
                         adapterBottomCenterBanners.setShimmerEnabled(false)
                         adapterBusinessType.setShimmerEnabled(false)
 
-//                        it.result.map {
-//                            Log.e(TAG, "setupCatalogsObserver: ${it.id}")
-//                        }
 
                         //Top and Trending
-                        it.result.filter {
-                            it.id == 3
-                        }.let {
-                            adapterTopTrending.setItemAndNotify(it.first().data as MutableList<Data>)
-                        }
-//                        shimmerTopTrending.hide()
-//                        rvTopTrending.show()
+                        notifyTopAndTrending(it)
 
 
                         ///By Categories
-                        it.result.filter {
-                            it.id == 147
-                        }.let {
-                            adapterCategories.setItemAndNotify(it.first().data as MutableList<Data>)
-                        }
+                        notifyByCategiriesFilter(it)
 
 
                         ///Center Bottom Banners
-                        it.result.filter {
-                            it.id == 148
-                        }.let {
-                            adapterBottomCenterBanners.setItemAndNotify(it.first().data as MutableList<Data>)
-                        }
+                        notifyCenterBottomBanners(it)
 
 //                        ///By Business Types
-                        it.result.filter {
-                            it.id == 13
-                        }.let {
-                            adapterBusinessType.setItemAndNotify(it.first().data as MutableList<Data>)
-                        }
+                        notifyByBusinessTypesFilters(it)
 
 
                     }
@@ -135,20 +104,41 @@ class HomeFragment(override val layoutRes: Int = R.layout.fragment_home) :
                     doToast(state.message)
                 }
                 is State.Loading -> {
-//                    adapterTopTrending.setShimmerEnabled(true)
-//                    adapterTopTrending.setItemAndNotify(Data.populateShimmerDataList())
-//
-//
-//                    adapterCategories.setShimmerEnabled(true)
-//                    adapterCategories.setItemAndNotify(Data.populateShimmerDataList())
-//
-//                    adapterBottomCenterBanners.setShimmerEnabled(true)
-//                    adapterBottomCenterBanners.setItemAndNotify(Data.populateShimmerDataList())
-//
-//                    adapterBusinessType.setShimmerEnabled(true)
-//                    adapterBusinessType.setItemAndNotify(Data.populateShimmerDataList())
+//                    showLoaderShimmer()
                 }
             }
+        }
+    }
+
+    private fun notifyByBusinessTypesFilters(it: CatalogsData) {
+        it.result.filter {
+            it.id == 13
+        }.let {
+            adapterBusinessType.setItemAndNotify(it.first().data as MutableList<Data>)
+        }
+    }
+
+    private fun notifyCenterBottomBanners(it: CatalogsData) {
+        it.result.filter {
+            it.id == 148
+        }.let {
+            adapterBottomCenterBanners.setItemAndNotify(it.first().data as MutableList<Data>)
+        }
+    }
+
+    private fun notifyByCategiriesFilter(it: CatalogsData) {
+        it.result.filter {
+            it.id == 147
+        }.let {
+            adapterCategories.setItemAndNotify(it.first().data as MutableList<Data>)
+        }
+    }
+
+    private fun notifyTopAndTrending(it: CatalogsData) {
+        it.result.filter {
+            it.id == 3
+        }.let {
+            adapterTopTrending.setItemAndNotify(it.first().data as MutableList<Data>)
         }
     }
 
@@ -157,79 +147,23 @@ class HomeFragment(override val layoutRes: Int = R.layout.fragment_home) :
 
 
             //Top Banners
-            adapterTopTopBanners.addOnItemClickHandler(object :
-                BaseAdapter.OnItemClickListener<BannerResult> {
-                override fun onItemClick(item: BannerResult) {
-                    val bannerMoreInfoDlg = BannerMoreInfoDialog()
-                    bannerMoreInfoDlg.arguments = bundleOf(
-                        ARG_KEY_BANNER_ITEM to item
-                    )
-                    bannerMoreInfoDlg.show(childFragmentManager, BannerMoreInfoDialog.TAG)
-                }
-            })
-            rvBanners.adapter = adapterTopTopBanners
-//            adapterBanners.setShimmerEnabled(true)
-//            adapterBanners.setItemAndNotify(BannersData.populateDShimmerDataList())
+            handlingAdapterTopBanners()
 
 
             ////Top and Trending
-            adapterTopTrending.addOnItemClickHandler(object :
-                BaseAdapter.OnItemClickListener<Data> {
-                override fun onItemClick(item: Data) {
-                    doToast(item.name ?: "")
-                }
-            })
-            rvTopTrending.adapter = adapterTopTrending
-
+            handlingAdapterTopAndTrending()
 
 
             ///Categories
-            adapterCategories.addOnItemClickHandler(object :
-                BaseAdapter.OnItemClickListener<Data> {
-                override fun onItemClick(item: Data) {
-                    doToast(item.name ?: "")
-                }
-            })
-            rvCategories.adapter = adapterCategories
-//            val spacingInPixels = resources.getDimensionPixelSize(R.dimen.spacing)
-//            rvCategories.addItemDecoration(SpacesItemDecoration(spacingInPixels))
-            /*val gridLayoutManager = GridLayoutManager(requireActivity(), 4)
-            rvCategories.layoutManager = gridLayoutManager
-            rvCategories.viewTreeObserver.addOnGlobalLayoutListener(
-                object : OnGlobalLayoutListener {
-                    override fun onGlobalLayout() {
-                        rvCategories.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                        val viewWidth: Int = rvCategories.measuredWidth
-                        val cardViewWidth =
-                            activity!!.resources.getDimension(R.dimen.dimen_category_width)
-                        val newSpanCount =
-                            floor((viewWidth / cardViewWidth).toDouble()).toInt()
-                        gridLayoutManager.spanCount = newSpanCount
-                        gridLayoutManager.requestLayout()
-                    }
-                })*/
-            rvCategories.addViewTreeObserver(resources.getDimension(R.dimen.dimen_category_width).toInt())
+            handlingAdapterByCategories()
 
 
             ///Center  Bottom Banners
-            adapterBottomCenterBanners.addOnItemClickHandler(object :
-                BaseAdapter.OnItemClickListener<Data> {
-                override fun onItemClick(item: Data) {
-                    doToast(item.name ?: "")
-                }
-            })
-            rvCenterBanners.adapter = adapterBottomCenterBanners
+            handlingAdapterCenterBottomBanners()
 
 
             ///By Business Type
-            adapterBusinessType.addOnItemClickHandler(object :
-                BaseAdapter.OnItemClickListener<Data> {
-                override fun onItemClick(item: Data) {
-                    doToast(item.name ?: "")
-                }
-            })
-            rvBusinessTypes.adapter = adapterBusinessType
-            rvBusinessTypes.addViewTreeObserver(resources.getDimension(R.dimen.dimen_business_type_width).toInt())
+            handlingAdapterByBusinessTypeFilter()
 
 
 
@@ -240,21 +174,83 @@ class HomeFragment(override val layoutRes: Int = R.layout.fragment_home) :
             setupCatalogsObserver()
 
 
-
-
-            adapterTopTrending.setShimmerEnabled(true)
-            adapterTopTrending.setItemAndNotify(Data.populateShimmerDataList())
-
-
-            adapterCategories.setShimmerEnabled(true)
-            adapterCategories.setItemAndNotify(Data.populateShimmerDataList())
-
-            adapterBottomCenterBanners.setShimmerEnabled(true)
-            adapterBottomCenterBanners.setItemAndNotify(Data.populateShimmerDataList())
-
-            adapterBusinessType.setShimmerEnabled(true)
-            adapterBusinessType.setItemAndNotify(Data.populateShimmerDataList())
+            showLoaderShimmer()
         }
+    }
+
+    private fun FragmentHomeBinding.handlingAdapterByBusinessTypeFilter() {
+        adapterBusinessType.addOnItemClickHandler(object :
+            BaseAdapter.OnItemClickListener<Data> {
+            override fun onItemClick(item: Data) {
+                doToast(item.name ?: "")
+            }
+        })
+        rvBusinessTypes.adapter = adapterBusinessType
+        rvBusinessTypes.addViewTreeObserver(
+            resources.getDimension(R.dimen.dimen_business_type_width).toInt()
+        )
+    }
+
+    private fun FragmentHomeBinding.handlingAdapterCenterBottomBanners() {
+        adapterBottomCenterBanners.addOnItemClickHandler(object :
+            BaseAdapter.OnItemClickListener<Data> {
+            override fun onItemClick(item: Data) {
+                doToast(item.name ?: "")
+            }
+        })
+        rvCenterBanners.adapter = adapterBottomCenterBanners
+    }
+
+    private fun FragmentHomeBinding.handlingAdapterByCategories() {
+        adapterCategories.addOnItemClickHandler(object :
+            BaseAdapter.OnItemClickListener<Data> {
+            override fun onItemClick(item: Data) {
+                doToast(item.name ?: "")
+            }
+        })
+        rvCategories.adapter = adapterCategories
+        rvCategories.addViewTreeObserver(
+            resources.getDimension(R.dimen.dimen_category_width).toInt()
+        )
+    }
+
+    private fun FragmentHomeBinding.handlingAdapterTopAndTrending() {
+        adapterTopTrending.addOnItemClickHandler(object :
+            BaseAdapter.OnItemClickListener<Data> {
+            override fun onItemClick(item: Data) {
+                doToast(item.name ?: "")
+            }
+        })
+        rvTopTrending.adapter = adapterTopTrending
+    }
+
+    private fun FragmentHomeBinding.handlingAdapterTopBanners() {
+        adapterTopTopBanners.addOnItemClickHandler(object :
+            BaseAdapter.OnItemClickListener<BannerResult> {
+            override fun onItemClick(item: BannerResult) {
+                val bannerMoreInfoDlg = BannerMoreInfoDialog()
+                bannerMoreInfoDlg.arguments = bundleOf(
+                    ARG_KEY_BANNER_ITEM to item
+                )
+                bannerMoreInfoDlg.show(childFragmentManager, BannerMoreInfoDialog.TAG)
+            }
+        })
+        rvBanners.adapter = adapterTopTopBanners
+    }
+
+    private fun showLoaderShimmer() {
+        adapterTopTrending.setShimmerEnabled(true)
+        adapterTopTrending.setItemAndNotify(Data.populateShimmerDataList())
+
+
+        adapterCategories.setShimmerEnabled(true)
+        adapterCategories.setItemAndNotify(Data.populateShimmerDataList())
+
+        adapterBottomCenterBanners.setShimmerEnabled(true)
+        adapterBottomCenterBanners.setItemAndNotify(Data.populateShimmerDataList())
+
+        adapterBusinessType.setShimmerEnabled(true)
+        adapterBusinessType.setItemAndNotify(Data.populateShimmerDataList())
     }
 
 
